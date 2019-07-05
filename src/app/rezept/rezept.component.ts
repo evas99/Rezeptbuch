aa941bd63878fd7e788d2917896b792774aae98d
Eva Strunz <Eva@Evas-MacBook.local>
2019-07-05 10:38:04 +0200

    created component Rezept

---
+++ src/app/rezept/rezept.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rezept',
+  templateUrl: './rezept.component.html',
+  styleUrls: ['./rezept.component.css']
+})
+export class RezeptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
