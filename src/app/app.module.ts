6914fdb937f1bb1c247e7cfca9142cbd0d625c40
Eva Strunz <Eva@Evas-MacBook.local>
2019-07-05 10:36:30 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RezeptComponent } from './rezept/rezept.component';
+import { ZutatenlisteComponent } from './zutatenliste/zutatenliste.component';
+import { EinkaufslisteComponent } from './einkaufsliste/einkaufsliste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RezeptComponent,
+    ZutatenlisteComponent,
+    EinkaufslisteComponent
   ],
   imports: [
     BrowserModule,
